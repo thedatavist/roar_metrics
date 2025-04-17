6afc4a447e2767fa088c99a374dea72a0280ec11
dbmurray <darragh.murray@gmail.com>
2025-04-18 07:47:49 +1000

    Finished beeswarm

--- heroes_zeros/2 Code/generate_heroes_zeros.R
+++ heroes_zeros/2 Code/generate_heroes_zeros.R
@@ -62,46 +62,83 @@ x_max <- ceiling(max(beeswarm_player_stats$ratingPoints)/5)*5
 rating_colours <- c("Top 5"="#0E6ECE","Bottom 5"="#F56580","Other"="#BCBFBE")
 
 # 4. Build plot with tiny fonts -----------------------------------------------
-swarm_plot <- ggplot(beeswarm_player_stats,
-                     aes(x = ratingPoints, y = 0, colour = rating_group)) +
-  geom_quasirandom(aes(fill = rating_group, alpha = 0.4),
-                   method = "quasirandom", width = 0.3, size = 3, stroke = 0.8, shape = 21) +
-  scale_y_continuous(NULL, breaks = NULL, limits = c(-0.5, 0.5)) +
-  scale_x_continuous(breaks = seq(x_min, x_max, by = 5),
-                     minor_breaks = NULL, position = "top") +
+swarm_plot <- ggplot(beeswarm_player_stats, aes(x = ratingPoints, y = 0, colour = rating_group)) +
+  geom_quasirandom(
+    aes(fill = rating_group, alpha = 0.4),
+    method = "quasirandom",
+    width  = 0.3,
+    size   = 3,
+    stroke = 0.8,
+    shape  = 21
+  ) +
+  scale_y_continuous(
+    NULL,
+    breaks  = NULL,
+    limits  = c(-0.5, 0.5),
+    expand  = expansion(add = 0.02)
+  ) +
+  scale_x_continuous(
+    breaks       = seq(x_min, x_max, by = 5),
+    minor_breaks = NULL,
+    position     = "bottom"
+  ) +
   scale_fill_manual(values = rating_colours) +
   scale_colour_manual(values = rating_colours) +
   labs(
-    title    = "<b>Footy Heroes and Zeros</b> |<span style='font-weight:10;'>AFL 2025, Round 5</span>",
-    subtitle = "Beeswarm of AFL player ratings (≥25% time on ground), highlighting the <span style='color:#0E6ECE; font-weight:700;'>top 5</span> and <span style='color:#F56580; font-weight:700;'>bottom 5</span> of the round.",
+    title    = "<b>Footy Heroes and Zeros</b> |<span style='font-weight:100;'>AFL 2025, Round 5</span>",
+    subtitle = "Beeswarm distribution of AFL player ratings (≥25% time on ground), highlighting the <span style='color:#0E6ECE;'><b>Top 5</b></span> & <span style='color:#F56580;'><b>bottom 5</b></span> of the round.",
     x        = "Player Rating Points",
-    caption  = "Visual: Darragh Murray | Data: fitzRoy"
+    caption  = "Visual: Darragh Murray for <span><b>Roar Metrics</b></span> | Data: fitzRoy"
   ) +
   theme_minimal(base_family = "roboto", base_size = 12) +
   theme(
-    plot.title = element_markdown(
-      size       = 18,
-      family     = "bebas_neue",        # or exactly "bebas neue"
-      margin     = margin(b = 10),      # ← up from 10 to 25
-      lineheight = 1
+    # Title in Bebas Neue, with extra bottom margin
+    plot.title       = element_markdown(
+      size       = 22,
+      family     = "bebas_neue", 
+      lineheight = 1,
+      margin     = margin(b = 5)
     ),
-    plot.subtitle = element_markdown(
-      size   =  8,
+    # Subtitle in Roboto, padded from title
+    plot.subtitle    = element_markdown(
+      size       = 10,
+      family     = "roboto",
+      lineheight = 1.1,
+      margin     = margin(t = 5, b = 5)
+    ),
+    # switch to element_markdown for the caption
+    plot.caption = element_markdown(
+      size   = 6,
       family = "roboto",
-      margin = margin(t =  5, b = 16)   # a little padding above the subtitle
+      colour = "grey50",
+      hjust  = 1
     ),
-    axis.title.x.top      = element_text(size = 8, margin = margin(t=10, b = 10)),
-    axis.text.x           = element_text(size = 8),
-    plot.caption          = element_text(size = 6, family = "roboto", colour = "grey50", hjust = 1),
-    legend.position       = "none",
-    panel.grid.major.x    = element_line(size = 0.2, linetype = "dotted", colour = "#898989"),
-    panel.grid.major.y    = element_blank(),
-    panel.grid.minor      = element_blank(),
-    plot.background       = element_rect(fill = "#f5f5f5", colour = NA),
-    panel.background      = element_rect(fill = "#f5f5f5", colour = NA),
-    plot.margin           = margin(8, 16, 8, 16)
+    # Move axis title to bottom, remove top title
+    axis.title.x.top    = element_blank(),
+    axis.title.x.bottom = element_text(
+      size   = 8,
+      margin = margin(t = 6, b = 4)
+    ),
+    
+    axis.text.x      = element_text(size = 8),
+    legend.position     = "none",
+    
+    # Lighter, slightly thicker gridlines for print
+    panel.grid.major.x  = element_line(
+      linetype = "dotted",
+      size     = 0.25,
+      colour   = "#AAAAAA"
+    ),
+    panel.grid.major.y  = element_blank(),
+    panel.grid.minor    = element_blank(),
+    
+    # Background & margins
+    panel.background    = element_rect(fill = "#f5f5f5", colour = NA),
+    plot.background     = element_rect(fill = "#f5f5f5", colour = NA),
+    plot.margin         = margin(t = 15, r = 20, b = 15, l = 20)
   ) +
   coord_cartesian(xlim = c(x_min, x_max))
+
 
 final_plot <- swarm_plot +
   geom_text(
@@ -115,10 +152,10 @@ final_plot <- swarm_plot +
   ) +
   geom_curve(
     data       = labelled_players,
-    aes(x = ratingPoints, y = y_position*0.4 - sign(y_position)*0.03,
-        xend = ratingPoints, yend = 0 + sign(y_position)*0.02),
-    curvature = 0.2, arrow = arrow(length = unit(0.008, "npc"), type = "open"),
-    size = 0.4, colour = labelled_players$segment_colour, inherit.aes = FALSE
+    aes(x = ratingPoints, y = y_position*0.35 - sign(y_position)*0.01,
+        xend = ratingPoints, yend = 0 + sign(y_position)*0.03),
+    curvature = 0.2, arrow = arrow(length = unit(0.02, "npc"), type = "open"),
+     size = 0.4, colour = labelled_players$segment_colour, inherit.aes = FALSE
   )
 
 final_plot 
@@ -128,6 +165,6 @@ ggsave(
   filename = paste0("heroes_zeros_", round_to_analyse, ".png"),
   plot     = final_plot,
   device   = ragg::agg_png,
-  width    = 9, height = 5, units = "in", dpi = 300
+  width    = 8, height = 4.5, units = "in", dpi = 300
 )
 
